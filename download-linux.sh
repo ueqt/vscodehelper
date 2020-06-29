c07e6b69e54f2f3ced8e9857b01ecac65d207278
ueqt <ueqtxu@gmail.com>
2020-06-29 14:56:36 +0800

    fix download

---
+++ download-linux.sh
@@ -0,0 +1,7 @@
+# 先创建一个tmp临时目录，安装包都会下到里面
+# tr '\n' ' ' 多行转一行
+# | sh 将输出进行执行
+rm -rf extensions;
+mkdir extensions;
+cd extensions;
+code --list-extensions --show-versions | sed -r 's/(.*?)\.(.*?)@(.*)/https:\/\/marketplace.visualstudio.com\/_apis\/public\/gallery\/publishers\/\1\/vsextensions\/\2\/\3\/vspackage -o \1.\2-\3.vsix/' | tr '\n' ' ' | sed -r 's/(.*)/curl \1/' | sh
